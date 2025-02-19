9e2f2c7fb9b0bef02477a95a4840144b6e07869e
Clinton <clintonp0593@gmail.com>
2025-02-19 16:11:49 +0400

    Refactored code

--- src/genai/core/processing/ingestion.py
+++ src/genai/core/processing/ingestion.py
@@ -3,83 +3,120 @@ import time
 import json
 import requests
 import os
+from src.genai.core.connectors.http_connector import HTTPClient
 
-def divide_chunks(l, n): 
-    logger.info({"message":"divide_chunks ","data":{"l":l,"n":n}})
-    # looping till length l 
-    for i in range(0, len(l), n):  
-        yield l[i:i + n]    
+from typing import Any, Dict, List, Iterable, Literal
 
-def upload_in_chunks(index_name,batch_array,action="upload"):
-    logger.info({"message":"divide_chunks ","data":{"index_name":index_name,"batch_array":batch_array}})
-    try :
+def divide_chunks(l: List, n: int) -> Iterable[List]:
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def upload_in_chunks(
+    index_name: str, 
+    batch_array: List[Dict],
+    action: str = "upload"
+) -> List[bytes]:
+    """
+    Uploads a list of embeddings in chunks to Azure Search.
+    
+    Args:
+    index_name (str): The Azure Search index name.
+    batch_array (List[Dict]): The list of embeddings to be uploaded.
+    action (str, optional): The action to be performed on the index. Defaults to "upload".
+    
+    Returns:
+    List[bytes]: A list of the responses from Azure Search, one for each chunk.
+    """
+    try:
         chunked_list = list(divide_chunks(batch_array, BATCH_SIZE))
-        logger.info({ "message":"chunked_list ", "data":len(chunked_list)})
         result = []
         for list_item in enumerate(chunked_list):
             time.sleep(SLEEP_TIME_SEC)
-            uploaded_list = upload_embeddings(index_name,list_item[1],action)
-            logger.info({"message" :"upload_in_chunks result ","data" : uploaded_list})
+            uploaded_list = upload_embeddings(index_name, list_item[1], action)
             result.append(uploaded_list)
         return result  
     except Exception as e:
         raise e
 
-def upload_embeddings(index_name, embeddings_list,action="upload"):
-    logger.info({"message":"upload_embeddings ","data":{"index_name":index_name,"embeddings_list":embeddings_list}})	
+def upload_embeddings(
+    index_name: str, 
+    embeddings_list: List[Dict[str, Any]], 
+    action: str = "upload"
+) -> bytes:
+    """
+    Uploads a list of embeddings to Azure Search.
+
+    Args:
+    index_name (str): The Azure Search index name.
+    embeddings_list (List[Dict[str, Any]]): The list of embeddings to be uploaded.
+    action (str, optional): The action to be performed on the index. Defaults to "upload".
+
+    Returns:
+    bytes: The response from Azure Search.
+    """
     url = f"https://{AZURE_SEARCH_SERVICE}.search.windows.net/indexes/{index_name}/docs/index"
-    querystring = {"api-version":"2023-07-01-Preview"}
+    querystring = {"api-version": "2023-07-01-Preview"}
     headers = {
         'api-key': AZURE_SEARCH_KEY,
         'content-type': "application/json"
     }
     proxies = { 
-        "http"  : PROXY_URL,
-        "https"  : PROXY_URL
+        "http": PROXY_URL,
+        "https": PROXY_URL
     }
-    logger.info({"message": "check len", "data":len(embeddings_list)})
-    for idx,embed_obj in enumerate(embeddings_list):
+    for embed_obj in embeddings_list:
         embed_obj.update({"@search.action": action})
 
-    logger.info({"message":"upload_embeddings ","data":{"url":url,"proxies":proxies}})
-    payload = {"value" : embeddings_list}
-    logger.info({"message" :"payload ","data" : payload})
+    payload = {"value": embeddings_list}
     try:
-        response = requests.request("POST", url, data=json.dumps(payload), headers=headers, params=querystring,proxies=proxies if ENABLE_PROXY == "True" else None,verify=False)
-        logger.info({"message" :"upload_embeddings response ","data" : {"response":response}})
+        response = HTTPClient().execute_request(
+            method='POST',
+            url=url,
+            headers=headers,
+            body=payload,
+            params=querystring,
+            proxies=proxies if ENABLE_PROXY == "True" else None,
+            verify=False
+        )
         return response.content
     except Exception as e:
-        logger.error({"message":"error in upload embedding","data":e.__str__(),"statusCode":500})
+        raise e
 
-
-def start_ingestion(index_name,path):
-    logger.info( {"message":"start_ingestion","data":{"request":index_name,"path":path}})
+def start_ingestion(index_name, path):
     upload_result = []
-    try :
-        for idx,file in enumerate(os.listdir(path)):
-            logger.info({"message" :"starting to ingest ","data" : file})
-            with open(path+file) as json_file:
-                embeddings_list = json.load(json_file)
-            json_file.close()    
-            upload_result.append(upload_in_chunks(index_name, embeddings_list["upload_embeddings_list"]))
-            logger.info( {"message":"start_ingestion","data":{"upload_result":upload_result,"len":len(upload_result)}})
+    try:
+        files = os.scandir(path)
+        for entry in files:
+            if entry.is_file():
+                with open(entry.path) as json_file:
+                    embeddings_list = json.load(json_file)
+                upload_result.append(upload_in_chunks(index_name, embeddings_list["upload_embeddings_list"]))
         return upload_result
     except FileNotFoundError as ferror:
-        logger.info({"message" :"FileNotFoundError ","data" : ferror})
-        return HTTPException(detail = FileNotFoundError)
+        raise HTTPException(detail=str(ferror))
 
-def start_ingestion_v1(index_name,file_path,action="upload"):
-    logger.info( {"message":"start_ingestion","data":{"request":index_name,"path":file_path}})
-    upload_result = []
-    try :
-        for idx,file in enumerate(os.listdir(file_path)):
-            logger.info({"message" :"starting to ingest ","data" : file})
-            with open(file_path+"/"+file) as json_file:
-                embeddings_list = json.load(json_file)
-            json_file.close()    
-            upload_result.append(upload_in_chunks(index_name, embeddings_list["upload_embeddings_list"],action))
-            logger.info( {"message":"start_ingestion","data":{"upload_result":upload_result,"len":len(upload_result)}})
+def start_ingestion_v1(
+    index_name: str, 
+    file_path: str, 
+    action: Literal["upload", "merge", "mergeOrUpload"] = "upload"
+) -> List[bytes]:
+    """
+    Uploads a list of embeddings to Azure Search.
+
+    Args:
+    index_name (str): The Azure Search index name.
+    file_path (str): The path to the folder containing the JSON files.
+    action (Literal["upload", "merge", "mergeOrUpload"], optional): The action to be performed on the index. Defaults to "upload".
+
+    Returns:
+    List[bytes]: The responses from Azure Search.
+    """
+    upload_result: List[bytes] = []
+    try:
+        for file in os.listdir(file_path):
+            with open(os.path.join(file_path, file)) as json_file:
+                embeddings_list: Dict[str, List[Dict[str, Any]]] = json.load(json_file)
+            upload_result.append(upload_in_chunks(index_name, embeddings_list["upload_embeddings_list"], action))
         return upload_result
     except FileNotFoundError as ferror:
-        logger.info({"message" :"FileNotFoundError ","data" : ferror})
-        raise ferror
+        raise HTTPException(detail=str(ferror))
